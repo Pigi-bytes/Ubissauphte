0b3effb9faa7a1bc195a1ec3128603a40cd2fb73
Barth <derouaultbarthelemy@protonmail.com>
2025-01-13 13:02:40 +0100

    ajout de l'object manager

---
+++ src/utils/objectManager.c
@@ -0,0 +1,49 @@
+#include "objectManager.h"
+
+t_objectManager* initObjectManager(t_objectType type, void (*freeFunc)(void*)) {
+    DEBUG_PRINT("Création d'un gestionnaire d'objets de type %d\n", type);
+    t_objectManager* manager = malloc(sizeof(t_objectManager));
+    manager->freeFunc = freeFunc;
+    manager->count = 0;
+    manager->capacity = INITIAL_CAPACITY;
+    manager->items = malloc(manager->capacity * sizeof(t_typedObject*));
+    manager->type = type;
+    DEBUG_PRINT("[MALLOC] Gestionnaire d'objets créé avec succès à l'adresse : %p\n", manager);
+    return manager;
+}
+
+t_typedObject* createTypedObject(t_objectType type, void* data) {
+    t_typedObject* obj = malloc(sizeof(t_typedObject));
+    obj->type = type;
+    obj->data = data;
+    DEBUG_PRINT("[MALLOC] Objet de type %d créé avec succès à l'adresse : %p\n", type, obj);
+    return obj;
+}
+
+void addObject(t_objectManager* manager, t_typedObject* object) {
+    DEBUG_PRINT("Ajout de l'objet d'adress %p au gestionnaire à l'adresse : %p \n", object, manager);
+    if (manager->type != object->type) {
+        fprintf(stderr, "Erreur: Tentative d'ajout d'un objet de type %d incorrect dans manager de type %d\n", object->type, manager->type);
+        exit(1);
+    }
+    if (manager->count >= manager->capacity) {
+        DEBUG_PRINT("Capacité du gestionnaire atteinte. Redimensionnement à %d...\n", manager->capacity * 2);
+        manager->capacity *= 2;
+        manager->items = realloc(manager->items, manager->capacity * sizeof(t_typedObject*));
+    }
+    manager->items[manager->count] = object;
+    manager->count++;
+    DEBUG_PRINT("Objet ajouté. Nombre d'objets dans le gestionnaire : %d, capacité : %d \n", manager->count, manager->capacity);
+}
+
+void freeObjectManager(t_objectManager* manager) {
+    DEBUG_PRINT("[FREE] Libération des ressources du gestionnaire d'objets à l'adresse : %p\n", manager);
+    for (int i = 0; i < manager->count; ++i) {
+        DEBUG_PRINT("[FREE] Libération des ressources de l'objet à l'adresse : %p\n", manager->items[i]);
+        manager->freeFunc(manager->items[i]->data);
+        free(manager->items[i]);
+    }
+    free(manager->items);
+    free(manager);
+    DEBUG_PRINT("Gestionnaire d'objets libéré avec succès.\n");
+}
---
+++ src/utils/objectManager.h
@@ -0,0 +1,36 @@
+#ifndef OBJECT_MANAGER_H
+#define OBJECT_MANAGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../debug.h"
+
+#define INITIAL_CAPACITY 10
+#define ADD_TYPED_OBJECT(manager, type, value_ptr) addObject(manager, createTypedObject(type, value_ptr))
+
+typedef enum {
+    INT_TYPE,
+    POINT_TYPE,
+    BUTTON_TYPE,
+} t_objectType;
+
+typedef struct {
+    t_objectType type;
+    void* data;
+} t_typedObject;
+
+typedef struct {
+    t_typedObject** items;
+    int count;
+    int capacity;
+    t_objectType type;
+    void (*freeFunc)(void*);
+} t_objectManager;
+
+t_objectManager* initObjectManager(t_objectType type, void (*freeFunc)(void*));
+t_typedObject* createTypedObject(t_objectType type, void* data);
+void addObject(t_objectManager* manager, t_typedObject* object);
+void freeObjectManager(t_objectManager* manager);
+
+#endif  // OBJECT_MANAGER_H
