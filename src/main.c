a04ec3eb949522043a890b79a62a11e4c39cb1f0
Souleymane <souleymanebarry8872@gmail.com>
2025-01-31 13:44:24 +0100

    test move

--- src/main.c
+++ src/main.c
@@ -2,11 +2,9 @@
 
 #include "./move.h"
 
-
 #define window_width 1280
 #define window_height 960
 #define NUM_OBSTACLES 5
-
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
@@ -14,15 +12,15 @@ int main(int argc, char* argv[]) {
     srand(time(NULL));
     SDL_Window* window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture* texture_background = loadImage("../../Ubissauphte/assets/imgs/donjon_sdl.bmp", renderer);
+    SDL_Texture* texture_background = loadImage("../Ubissauphte/assets/imgs/donjon_sdl.bmp", renderer);
 
     t_control fleche = {SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT};
     t_control j2 = {SDL_SCANCODE_W, SDL_SCANCODE_S, SDL_SCANCODE_A, SDL_SCANCODE_D};
     t_control j3 = {SDL_SCANCODE_I, SDL_SCANCODE_K, SDL_SCANCODE_J, SDL_SCANCODE_L};
 
-    t_joueur* player1 = createplayer(&fleche, loadImage("../../Ubissauphte/assets/imgs/personnage_sdl.bmp", renderer), (SDL_Rect){400, 300, 100, 100});
-    t_joueur* player2 = createplayer(&j2, loadImage("../../Ubissauphte/assets/imgs/squid-game_sdl.bmp", renderer), (SDL_Rect){400, 400, 100, 150});
-    t_joueur* player3 = createplayer(&j3, loadImage("../../Ubissauphte/assets/imgs/streetfighter_sdl.bmp", renderer), (SDL_Rect){400, 500, 150, 150});
+    t_joueur* player1 = createplayer(&fleche, loadImage("../Ubissauphte/assets/imgs/personnage_sdl.bmp", renderer), (SDL_Rect){400, 300, 100, 100});
+    t_joueur* player2 = createplayer(&j2, loadImage("../Ubissauphte/assets/imgs/squid-game_sdl.bmp", renderer), (SDL_Rect){400, 400, 100, 150});
+    t_joueur* player3 = createplayer(&j3, loadImage("../Ubissauphte/assets/imgs/streetfighter_sdl.bmp", renderer), (SDL_Rect){400, 500, 150, 150});
 
     Obstacle obstacles[NUM_OBSTACLES];
     for (int i = 0; i < NUM_OBSTACLES; i++) {
