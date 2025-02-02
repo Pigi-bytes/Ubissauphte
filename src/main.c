9ee9994d9b268a1f498a0a7df661866a40c331d3
Barth <derouaultbarthelemy@protonmail.com>
2025-02-02 18:16:35 +0100

    ajout du main pour tester

--- src/main.c
+++ src/main.c
@@ -1,53 +1,44 @@
-#include <stdio.h>
-#include <time.h>
 #include <SDL2/SDL.h>
-#include "io/input.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "genmap.h"
+#include "io/input.h"
 #include "mapview.h"
 
-#include <unistd.h>
-
-#define window_width 1280
-#define window_height 960
-#define EXIT_SUCCESS 0
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 960
 
 int main() {
-    int niveau = 15;
-
+    int nbRoom = 15;  // Nombre de salles
 
     SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window* window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    t_input input;
-    initInput(&input, window_width, window_height);
-
-    int *tabCoordsX = malloc(sizeof(int)*niveau);
-    int *tabCoordsY = malloc(sizeof(int)*niveau);
-
-    t_salle **listRoom = genMap(niveau, tabCoordsX, tabCoordsY);
-
-        
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
+    t_mapAffichage map;
+    SDL_Rect* roomCoords = malloc(sizeof(SDL_Rect) * nbRoom);
     clock_t start = clock();
-    printMap(tabCoordsX, tabCoordsY, listRoom, niveau, renderer);
+    t_salle** listRoom = genMap(nbRoom, roomCoords);
+    generateMap(roomCoords, listRoom, nbRoom, &map);
     clock_t end = clock();
+
     double duration = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-    printf("genMapCoords prend %.2fms\n", duration);
+    printf("printMap prend %.2fms\n", duration);
 
-
-
-    SDL_RenderPresent(renderer);
-
-    while(!input.quit){
+    t_input input;
+    initInput(&input, WINDOW_WIDTH, WINDOW_HEIGHT);
+    while (!input.quit) {
         updateInput(&input);
+        affichage(renderer, &map);
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyWindow(window);
+    free(roomCoords);
+    free(listRoom);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return EXIT_SUCCESS;
 }
