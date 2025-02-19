47b030d20f3b036e934d99d1f91977da2fbbd61e
Souleymane <souleymanebarry8872@gmail.com>
2025-02-19 11:20:43 +0100

    Ajout du doxygène et amélioration du fichierLoader

---
+++ src/io/fichierLoader.c
@@ -0,0 +1,128 @@
+
+#include "fichierLoader.h"
+void saveFichier(t_fichier *fichier, char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        perror("Erreur lors de l'ouverture de fichier");
+        exit(EXIT_FAILURE);
+    }
+
+    t_block *Block;
+    t_pairData *data;
+    for (int i = 0; i < fichier->blockManager->count; i++) {
+        Block = (t_block *)getObject(fichier->blockManager, i);
+        for (int j = 0; j < Block->pairManager->count; j++) {
+            data = (t_pairData *)getObject(Block->pairManager, j);
+            fprintf(file, "%s: %s\n", data->key, data->value);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+}
+
+void addBlock(t_fichier *fichier, t_block *Block) {
+    ADD_TYPED_OBJECT(fichier->blockManager, BLOCK_TYPE, Block);
+}
+
+void addPairData(t_block *block, t_pairData *pair) {
+    ADD_TYPED_OBJECT(block->pairManager, TYPE_PAIR, pair);
+}
+
+t_fichier *chargerFichier(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Erreur d'ouverture du fichier");
+        exit(EXIT_FAILURE);
+    }
+    t_fichier *fichier = malloc(sizeof(t_fichier));
+    // Initialisation d'une zone de stockage pour le premier block
+    fichier->blockManager = initObjectManager(BLOCK_TYPE, blockFreeFunc);
+
+    t_block *currentBlock = NULL;
+    char line[MAX_LINE_LENGTH];
+
+    // Lecture du fichier
+    while (fgets(line, MAX_LINE_LENGTH, file)) {
+        line[strcspn(line, "\n")] = 0;
+        // Fin du block en cas de ligne vide puis stockage dans la zone prévu pour
+        if (strlen(line) == 0) {
+            if (currentBlock) {
+                ADD_TYPED_OBJECT(fichier->blockManager, BLOCK_TYPE, currentBlock);
+                currentBlock = NULL;
+            }
+        } else {  // Allocation de mémoire pour chaque case du tableau t_block
+            if (!currentBlock) {
+                currentBlock = malloc(sizeof(t_block));
+                currentBlock->pairManager = initObjectManager(TYPE_PAIR, pairFreeFunc);
+            }
+
+            // Séparation de la cle et de la valeur grace au ':'
+            char *separator = strchr(line, ':');
+            if (separator) {
+                *separator = '\0';            // On coupe la chaine ici ("cle\0 valeur") -> avec un char de fin de chaine
+                char *key = line;             // Partie cle a gauche du ': '
+                char *value = separator + 1;  // Partie valeur à droite du ': '
+
+                // Allocation de mémoire pour chaque t_pairData
+                t_pairData *pair = malloc(sizeof(t_pairData));
+                pair->key = strdup(key);
+                // S'il y a un espace présent entre le ':' et la valeur on le supprime
+                pair->value = strdup(value + (value[0] == ' ' ? 1 : 0));
+
+                ADD_TYPED_OBJECT(currentBlock->pairManager, TYPE_PAIR, pair);
+            }
+        }
+    }
+    if (currentBlock) {
+        ADD_TYPED_OBJECT(fichier->blockManager, BLOCK_TYPE, currentBlock);
+    }
+
+    fclose(file);
+    return fichier;
+}
+
+bool getValue(t_block *block, char *name, void *result, t_valueType type) {
+    for (int i = 0; i < block->pairManager->count; i++) {
+        t_pairData *pair = (t_pairData *)getObject(block->pairManager, i);
+        if (strcmp(pair->key, name) == 0) {
+            switch (type) {
+                case INT:
+                    *(int *)result = atoi(pair->value);
+                    return true;
+                case FLOAT:
+                    *(float *)result = (float)atof(pair->value);
+                    return true;
+                case DOUBLE:
+                    *(double *)result = atof(pair->value);
+                    return true;
+                case STRING:
+                    strcpy((char *)result, pair->value);
+                    return true;
+                default:
+                    printf("erreur sur le type");
+                    return false;
+            }
+        }
+    }
+    return false;
+}
+
+void blockFreeFunc(void *data) {
+    t_block *block = (t_block *)data;
+    freeObjectManager(block->pairManager);
+    free(block);
+}
+
+void pairFreeFunc(void *data) {
+    t_pairData *pair = (t_pairData *)data;
+    free(pair->key);
+    free(pair->value);
+    free(pair);
+}
+
+void freeFichier(t_fichier *fichier) {
+    if (fichier && fichier->blockManager) {
+        freeObjectManager(fichier->blockManager);
+    }
+    free(fichier);
+}
