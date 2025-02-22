f90316f15018e8edc8c659652ac7abfa493c698e
Baptcarf <baptiste.carfantan72@gmail.com>
2025-02-22 21:41:21 +0100

    ajout de fonction pour le  lissage

--- src/tileEngine/lissage.c
+++ src/tileEngine/lissage.c
@@ -14,8 +14,16 @@ int ajoutArriere(int i, int j, int mat[HEIGHT][WIDTH]) {
     return verifVoisinPlafond(i + 1, j, mat) && verifVoisinSol(i + 2, j, mat) && verifVoisinSol(i - 1, j, mat);
 }
 
-int supressionLateral(int i, int j, int mat[HEIGHT][WIDTH]) {
+int probleme_diag_bas_vers_haut(int i, int j, int mat[HEIGHT][WIDTH]) {
     return verifVoisinSol(i + 1, j - 1, mat) && verifVoisinSol(i - 1, j + 1, mat) && (verifVoisinPlafond(i, j + 1, mat) || verifVoisinPlafond(i, j - 1, mat));
+}
+
+int supressionLateral2(int i, int j, int mat[HEIGHT][WIDTH]) {
+    return verifVoisinSol(i - 1, j + 1, mat) && verifVoisinSol(i + 2, j - 1, mat) && (verifVoisinPlafond(i, j + 1, mat) || verifVoisinPlafond(i, j - 1, mat));
+}
+
+int probleme_diag_haut_vers_bas(int i, int j, int mat[HEIGHT][WIDTH]) {
+    return verifVoisinSol(i - 1, j - 1, mat) && verifVoisinSol(i + 2, j + 1, mat) && (verifVoisinPlafond(i, j + 1, mat) && verifVoisinPlafond(i, j - 1, mat));
 }
 
 void lissage(int mat[HEIGHT][WIDTH]) {
@@ -27,9 +35,14 @@ void lissage(int mat[HEIGHT][WIDTH]) {
                 } else if (ajoutArriere(i, j, mat)) {
                     mat[i][j] = 0;
                     mat[i + 1][j] = 0;
-                } else if (supressionLateral(i, j, mat)) {
+                } else if (probleme_diag_bas_vers_haut(i, j, mat)) {
                     mat[i + 1][j - 1] = 1;
                     if (inMat(i + 2, j - 1)) mat[i + 2][j - 1] = 1;
+                } else if (supressionLateral2(i, j, mat)) {
+                    mat[i + 1][j - 1] = 1;
+                    if (inMat(i + 2, j - 1)) mat[i + 2][j - 1] = 1;
+                } else if (probleme_diag_haut_vers_bas(i, j, mat)) {
+                    if (inMat(i + 2, j - +1)) mat[i + 2][j + 1] = 1;
                 }
             }
         }
--- src/tileEngine/lissage.h
+++ src/tileEngine/lissage.h
@@ -4,4 +4,7 @@ int caseSolo(int i, int j, int mat[HEIGHT][WIDTH]);
 int verifVoisinSol(int i, int j, int mat[HEIGHT][WIDTH]);
 int verifVoisinPlafond(int i, int j, int mat[HEIGHT][WIDTH]);
 int ajoutArriere(int i, int j, int mat[HEIGHT][WIDTH]);
+int probleme_diag_bas_vers_haut(int i, int j, int mat[HEIGHT][WIDTH]);
+int probleme_diag_coin(int i, int j, int mat[HEIGHT][WIDTH]);
+int probleme_diag_haut_vers_bas(int i, int j, int mat[HEIGHT][WIDTH]);
 void lissage(int mat[HEIGHT][WIDTH]);
