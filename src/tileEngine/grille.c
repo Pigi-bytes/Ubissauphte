e993a2d50f69620630585dd9b69adc20abc0dee4
Baptcarf <baptiste.carfantan72@gmail.com>
2025-02-27 23:34:07 +0100

    programme qui gère des grilles pour simplifier la compréhension de GeneRoom

---
+++ src/tileEngine/grille.c
@@ -0,0 +1,119 @@
+#include "grille.h"
+
+t_case* initCase(int i, int j, int val) {
+    t_case* c = malloc(sizeof(t_case));
+    c->i = i;
+    c->j = j;
+    c->val = val;
+    c->tiles = NULL;
+    return c;
+}
+
+t_grille* initGrille(int nbLigne, int nbColonne) {
+    t_grille* g = malloc(sizeof(t_grille));
+    if (!g) {
+        printf("erreur lors de l'allocationde la grille");
+        exit(EXIT_FAILURE);
+    }
+    g->grille = (t_case***)malloc(sizeof(t_case**) * nbLigne);
+
+    if (!(g->grille)) {
+        printf("erreur lors de l'allocation des ligne de la grille");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < nbLigne; i++) {
+        g->grille[i] = (t_case**)malloc(sizeof(t_case*) * nbColonne);
+        if (!(g->grille[i])) {
+            printf("erreur lors de l'allocation de la colonne de la grille");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < nbLigne; i++) {
+        for (int j = 0; j < nbColonne; j++) {
+            g->grille[i][j] = NULL;
+        }
+    }
+    g->nbLigne = nbLigne;
+    g->nbColonne = nbColonne;
+    return g;
+}
+
+void ajouterCase(t_grille* g, int i, int j, int val) {
+    if (g->grille[i][j] == NULL) {
+        g->grille[i][j] = initCase(i, j, val);
+    }
+}
+
+void freeGrille(t_grille* g) {
+    for (int i = 0; i < g->nbLigne; i++) {
+        for (int j = 0; j < g->nbColonne; j++) {
+            free(g->grille[i][j]);
+        }
+        free(g->grille[i]);
+    }
+    free(g->grille);
+    free(g);
+}
+
+t_grille* intToGrille(int** entier, int nbLigne, int nbColonne) {
+    t_grille* g = initGrille(nbLigne, nbColonne);
+    for (int i = 0; i < nbLigne; i++) {
+        for (int j = 0; j < nbColonne; j++) {
+            g->grille[i][j] = initCase(i, j, entier[i][j]);
+        }
+    }
+
+    for (int i = 0; i < nbLigne; i++) {
+        for (int j = 0; j < nbColonne; j++) {
+            if (inMat2(i - 1, j, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_HAUT] = g->grille[i - 1][j];
+            if (inMat2(i - 2, j, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_HAUT2] = g->grille[i - 2][j];
+            if (inMat2(i + 1, j, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_BAS] = g->grille[i + 1][j];
+            if (inMat2(i + 2, j, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_BAS2] = g->grille[i + 2][j];
+            if (inMat2(i, j - 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_GAUCHE] = g->grille[i][j - 1];
+            if (inMat2(i, j - 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_GAUCHE2] = g->grille[i][j - 2];
+            if (inMat2(i, j + 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DROIT] = g->grille[i][j + 1];
+            if (inMat2(i, j + 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DROIT2] = g->grille[i][j + 2];
+            if (inMat2(i + 1, j + 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_DROIT_BAS] = g->grille[i + 1][j + 1];
+            if (inMat2(i + 2, j + 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_DROIT_BAS2] = g->grille[i + 2][j + 2];
+            if (inMat2(i - 1, j + 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_DROIT_HAUT] = g->grille[i - 1][j + 1];
+            if (inMat2(i - 2, j + 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_DROIT_HAUT2] = g->grille[i - 2][j + 2];
+            if (inMat2(i + 1, j - 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_GAUCHE_BAS] = g->grille[i + 1][j - 1];
+            if (inMat2(i + 2, j - 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_DROIT_BAS2] = g->grille[i + 2][j + 2];
+            if (inMat2(i - 1, j - 1, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_GAUCHE_HAUT] = g->grille[i - 1][j + 1];
+            if (inMat2(i - 2, j - 2, nbLigne, nbColonne))
+                g->grille[i][j]->tabVoisin[VOISIN_DIAG_GAUCHE_HAUT2] = g->grille[i - 2][j + 2];
+        }
+    }
+    return g;
+}
+
+int main() {
+    int** mat = (int**)malloc(sizeof(int*) * 16);
+    for (int i = 0; i < 16; i++) {
+        mat[i] = (int*)malloc(sizeof(int) * 16);
+    }
+
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < 16; j++) {
+            mat[i][j] = 0;
+        }
+
+    t_grille* g = intToGrille(mat, 16, 16);
+    free(g);
+}
---
+++ src/tileEngine/grille.h
@@ -0,0 +1,37 @@
+#include "general.h"
+#include "liste_block.h"
+#define VOISIN_GAUCHE 0
+#define VOISIN_DROIT 1
+#define VOISIN_BAS 2
+#define VOISIN_HAUT 3
+#define VOISIN_DIAG_GAUCHE_HAUT 4
+#define VOISIN_DIAG_DROIT_HAUT 5
+#define VOISIN_DIAG_GAUCHE_BAS 6
+#define VOISIN_DIAG_DROIT_BAS 7
+
+#define VOISIN_GAUCHE2 8
+#define VOISIN_DROIT2 9
+#define VOISIN_BAS2 10
+#define VOISIN_HAUT2 11
+#define VOISIN_DIAG_GAUCHE_HAUT2 12
+#define VOISIN_DIAG_DROIT_HAUT2 13
+#define VOISIN_DIAG_GAUCHE_BAS2 14
+#define VOISIN_DIAG_DROIT_BAS2 15
+
+typedef struct s_case {
+    int i, j;
+    t_block* tiles;
+    int val;
+    struct s_case* tabVoisin[16];
+} t_case;
+
+typedef struct s_grille {
+    int nbLigne;
+    int nbColonne;
+    t_case*** grille;
+} t_grille;
+
+t_case* initCase(int i, int j, int val);
+t_grille* initGrille(int nbLigne, int nbColonne);
+void ajouterCase(t_grille* g, int i, int j, int val);
+void freeGrille(t_grille* g);
