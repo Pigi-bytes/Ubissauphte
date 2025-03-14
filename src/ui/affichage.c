a40d7f89c7bcb26979cb1e114e32e2b1f81b7aec
Souleymane <souleymanebarry8872@gmail.com>
2025-03-14 16:56:31 +0100

    noms plus adapté

--- src/ui/affichage.c
+++ src/ui/affichage.c
@@ -1,99 +1,105 @@
 #include "affichage.h"
 
-void calculPlayer(SDL_Rect* Player, t_input* input, char* nom) {
+void calculCasePlayer(SDL_Rect* casePlayer, t_input* input, char* nom) {
     if (strcmp(nom, "Perso") == 0) {
-        Player->h = input->windowWidth * 0.14;
-        Player->w = input->windowWidth * 0.16;
-        Player->x = input->windowWidth * 0.02;
-        Player->y = input->windowHeight * 0.08;
+        casePlayer->h = input->windowWidth * 0.14;
+        casePlayer->w = input->windowWidth * 0.16;
+        casePlayer->x = input->windowWidth * 0.02;
+        casePlayer->y = input->windowHeight * 0.08;
     }
 }
-void calculSlot(SDL_Rect* slot, SDL_Rect* comp, t_input* input, char* nom1, char* nom2) {
+void calculCaseSlots(SDL_Rect* comp, SDL_Rect* slot, t_input* input, char* nom1, char* nom2) {
     if (!strcmp(nom2, "arme") && !strcmp(nom1, "Perso")) {
-        comp->h = slot->h / 2 - slot->h * 0.05;
-        comp->w = slot->w / 2;
-        comp->x = slot->w + slot->x + input->windowWidth * 0.04;
-        comp->y = slot->y;
+        slot->h = comp->h / 2 - comp->h * 0.05;
+        slot->w = comp->w / 2;
+        slot->x = comp->w + comp->x + input->windowWidth * 0.04;
+        slot->y = comp->y;
     }
     if (!strcmp(nom2, "armure") && !strcmp(nom1, "arme")) {
-        comp->h = slot->h;
-        comp->w = slot->w;
-        comp->x = slot->x;
-        comp->y = slot->h + input->windowHeight * 0.08 + input->windowWidth * 0.13 * 0.1;
+        slot->h = comp->h;
+        slot->w = comp->w;
+        slot->x = comp->x;
+        slot->y = comp->h + input->windowHeight * 0.08 + input->windowWidth * 0.13 * 0.1;
     }
 
     if (!strcmp(nom2, "activable1") && !strcmp(nom1, "Perso")) {
-        comp->h = slot->h / 2;
-        comp->w = slot->w / 2 - slot->w * 0.05;
-        comp->x = slot->x;
-        comp->y = slot->h + slot->y + input->windowHeight * 0.04;
+        slot->h = comp->h / 2;
+        slot->w = comp->w / 2 - comp->w * 0.05;
+        slot->x = comp->x;
+        slot->y = comp->h + comp->y + input->windowHeight * 0.04;
     }
 
     if (!strcmp(nom2, "activable2") && !strcmp(nom1, "activable1")) {
-        comp->h = slot->h;
-        comp->w = slot->w;
-        comp->x = slot->w + input->windowWidth * 0.02 + input->windowWidth * 0.185 * 0.1;
-        comp->y = slot->y;
+        slot->h = comp->h;
+        slot->w = comp->w;
+        slot->x = comp->w + input->windowWidth * 0.02 + input->windowWidth * 0.185 * 0.1;
+        slot->y = comp->y;
     }
 }
 
-void calculDescrPlayer(SDL_Rect* slot1, SDL_Rect* slot2, SDL_Rect* player, SDL_Rect* descr, t_input* input) {
-    descr->h = player->h + slot2->h;
-    descr->w = player->w + slot1->w + input->windowWidth * 0.04;
-    descr->x = player->x;
-    descr->y = slot2->y + slot2->h + input->windowHeight * 0.04;
+void calculDescrStatsPlayer(SDL_Rect* slotDroit, SDL_Rect* slotBas, SDL_Rect* casePlayer, SDL_Rect* caseStatsPlayer, t_input* input) {
+    caseStatsPlayer->h = casePlayer->h + slotBas->h;
+    caseStatsPlayer->w = casePlayer->w + slotDroit->w + input->windowWidth * 0.04;
+    caseStatsPlayer->x = casePlayer->x;
+    caseStatsPlayer->y = slotBas->y + slotBas->h + input->windowHeight * 0.04;
 }
 
-void calculInventaire(SDL_Rect* inv, SDL_Rect* descr, t_input* input) {
-    inv->h = descr->h * 2 + input->windowHeight * 0.08;
-    inv->w = descr->w * 1.5;
-    inv->x = descr->w + input->windowWidth * 0.04;
+void calculInventaire(SDL_Rect* inv, SDL_Rect* statsPlayer, t_input* input) {
+    inv->h = statsPlayer->h * 2 + input->windowHeight * 0.08;
+    inv->w = statsPlayer->w * 1.5;
+    inv->x = statsPlayer->w + input->windowWidth * 0.04;
     inv->y = input->windowHeight * 0.08;
 }
 
-void calculDescrItem(SDL_Rect* inv, SDL_Rect* descrItem, t_input* input) {
-    descrItem->h = inv->h / 2 - input->windowHeight * 0.01;
-    descrItem->w = inv->w / 2.5;
-    descrItem->x = inv->x + inv->w + input->windowWidth * 0.04;
-    descrItem->y = inv->y;
+void calculStatsItem(SDL_Rect* inv, SDL_Rect* statsItem, t_input* input) {
+    statsItem->h = inv->h / 2 - input->windowHeight * 0.01;
+    statsItem->w = inv->w / 2.5;
+    statsItem->x = inv->x + inv->w + input->windowWidth * 0.04;
+    statsItem->y = inv->y;
 }
 
-void caculDescr(SDL_Rect* descrItem, SDL_Rect* descr, t_input* input) {
-    descr->h = descrItem->h * 2 / 3 - input->windowHeight * 0.01;
-    descr->w = descrItem->w;
-    descr->x = descrItem->x;
-    descr->y = descrItem->h + descrItem->y + input->windowHeight * 0.03;
+void caculDescrItem(SDL_Rect* statsItem, SDL_Rect* descrItem, t_input* input) {
+    descrItem->h = statsItem->h * 2 / 3 - input->windowHeight * 0.01;
+    descrItem->w = statsItem->w;
+    descrItem->x = statsItem->x;
+    descrItem->y = statsItem->h + statsItem->y + input->windowHeight * 0.03;
 }
 
-void calculEquiper(SDL_Rect* descrItem, SDL_Rect* descr, SDL_Rect* Equiper, t_input* input) {
-    Equiper->h = descrItem->h / 3 - input->windowHeight * 0.03;
-    Equiper->w = descr->w;
-    Equiper->x = descr->x;
-    Equiper->y = descr->y + descr->h + input->windowHeight * 0.03;
+void calculEquiper(SDL_Rect* statsItem, SDL_Rect* descrItem, SDL_Rect* Equiper, t_input* input) {
+    Equiper->h = statsItem->h / 3 - input->windowHeight * 0.03;
+    Equiper->w = descrItem->w;
+    Equiper->x = descrItem->x;
+    Equiper->y = descrItem->y + descrItem->h + input->windowHeight * 0.03;
 }
 
 void afficherInventaire(SDL_Renderer* renderer, t_input* input) {
     SDL_Rect casePerso;
-    SDL_Rect arme;
-    SDL_Rect armure;
-    SDL_Rect activable1;
-    SDL_Rect activable2;
-    SDL_Rect descrPlayer;
+    SDL_Rect caseArme;
+    SDL_Rect caseArmure;
+    SDL_Rect CaseActivable1;
+    SDL_Rect caseActivable2;
+    SDL_Rect statsPlayer;
     SDL_Rect inv;
+    SDL_Rect statsItem;
     SDL_Rect descrItem;
-    SDL_Rect descr;
     SDL_Rect equiper;
 
-    calculPlayer(&casePerso, input, "Perso");
-    calculSlot(&casePerso, &arme, input, "Perso", "arme");
-    calculSlot(&arme, &armure, input, "arme", "armure");
-    calculSlot(&casePerso, &activable1, input, "Perso", "activable1");
-    calculSlot(&activable1, &activable2, input, "activable1", "activable2");
-    calculDescrPlayer(&arme, &activable1, &casePerso, &descrPlayer, input);
-    calculInventaire(&inv, &descrPlayer, input);
-    calculDescrItem(&inv, &descrItem, input);
-    caculDescr(&descrItem, &descr, input);
-    calculEquiper(&descrItem, &descr, &equiper, input);
+    calculCasePlayer(&casePerso, input, "Perso");
+
+    calculCaseSlots(&casePerso, &caseArme, input, "Perso", "arme");
+    calculCaseSlots(&caseArme, &caseArmure, input, "arme", "armure");
+    calculCaseSlots(&casePerso, &CaseActivable1, input, "Perso", "activable1");
+    calculCaseSlots(&CaseActivable1, &caseActivable2, input, "activable1", "activable2");
+
+    calculDescrStatsPlayer(&caseArme, &CaseActivable1, &casePerso, &statsPlayer, input);
+
+    calculInventaire(&inv, &statsPlayer, input);
+
+    calculStatsItem(&inv, &statsItem, input);
+
+    caculDescrItem(&statsItem, &descrItem, input);
+
+    calculEquiper(&statsItem, &descrItem, &equiper, input);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
@@ -101,13 +107,13 @@ void afficherInventaire(SDL_Renderer* renderer, t_input* input) {
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderDrawRect(renderer, &casePerso);
-    SDL_RenderDrawRect(renderer, &arme);
-    SDL_RenderDrawRect(renderer, &armure);
-    SDL_RenderDrawRect(renderer, &activable1);
-    SDL_RenderDrawRect(renderer, &activable2);
-    SDL_RenderDrawRect(renderer, &descrPlayer);
+    SDL_RenderDrawRect(renderer, &caseArme);
+    SDL_RenderDrawRect(renderer, &caseArmure);
+    SDL_RenderDrawRect(renderer, &CaseActivable1);
+    SDL_RenderDrawRect(renderer, &caseActivable2);
+    SDL_RenderDrawRect(renderer, &statsPlayer);
     SDL_RenderDrawRect(renderer, &inv);
+    SDL_RenderDrawRect(renderer, &statsItem);
     SDL_RenderDrawRect(renderer, &descrItem);
-    SDL_RenderDrawRect(renderer, &descr);
     SDL_RenderDrawRect(renderer, &equiper);
 }
--- src/ui/affichage.h
+++ src/ui/affichage.h
@@ -8,12 +8,12 @@
 #define window_width 1280
 #define window_height 960
 
-void calculPlayer(SDL_Rect* Player, t_input* input, char* nom);
-void calculSlot(SDL_Rect* slot, SDL_Rect* comp, t_input* input, char* nom1, char* nom2);
-void calculDescrPlayer(SDL_Rect* slot1, SDL_Rect* slot2, SDL_Rect* player, SDL_Rect* descr, t_input* input);
-void calculInventaire(SDL_Rect* Inv, SDL_Rect* descr, t_input* input);
-void calculDescrItem(SDL_Rect* inv, SDL_Rect* descrItem, t_input* input);
-void caculDescr(SDL_Rect* descrItem, SDL_Rect* descr, t_input* input);
+void calculCasePlayer(SDL_Rect* casePlayer, t_input* input, char* nom);
+void calculCaseSlots(SDL_Rect* comp, SDL_Rect* slot, t_input* input, char* nom1, char* nom2);
+void calculDescrStatsPlayer(SDL_Rect* slotDroit, SDL_Rect* slotBas, SDL_Rect* casePlayer, SDL_Rect* caseStatsPlayer, t_input* input);
+void calculInventaire(SDL_Rect* Inv, SDL_Rect* statsPlayer, t_input* input);
+void calculStatsItem(SDL_Rect* inv, SDL_Rect* statsItem, t_input* input);
+void caculDescrItem(SDL_Rect* statsItem, SDL_Rect* descrItem, t_input* input);
 void afficherInventaire(SDL_Renderer* renderer, t_input* input);
 
 #endif
