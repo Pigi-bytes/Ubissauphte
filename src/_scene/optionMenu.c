713e30fb72b13177efa3a37a1ebf95a9867a1d4f
Baptcarf <baptiste.carfantan72@gmail.com>
2025-03-24 19:16:13 +0100

    affichage des fps dans le menu des options

--- src/_scene/optionMenu.c
+++ src/_scene/optionMenu.c
@@ -27,6 +27,7 @@ t_scene* createOptionMenu(t_context* context) {
     const uint8_t TEXTE_TYPE = registerType(registre, freeText, "text");
     const uint8_t VOLUME_TYPE = registerType(registre, freeBv, "volumme");
     const uint8_t COMMANDE_TYPE = registerType(registre, freeTouche, "commande");
+    const uint8_t FRAME_DISPLAY_TYPE = registerType(registre, freeFPSDisplay, "frameData");
 
     t_scene* scene = createScene(initObjectManager(registre), "option");
 
@@ -47,6 +48,7 @@ t_scene* createOptionMenu(t_context* context) {
     ADD_OBJECT_TO_SCENE(scene, CreerBarreVolume(creerRect(0.35f, (0.58f + 0.05f), 0.3f, 0.01f), creerRect((0.35f + 0.14f), (0.58f + 0.032f), 0.02f, 0.04f), GREEN, WHITE, creerFonction(bouttonClickQuit, FONCTION_PARAMS(context->input))), VOLUME_TYPE);
     ADD_OBJECT_TO_SCENE(scene, fpsButtonEcran, BUTTON_TYPE);
     ADD_OBJECT_TO_SCENE(scene, createButton(createTextOutline(context->renderer, "Menu Principal", context->font, BLACK, WHITE, 2), GREEN, WHITE, creerRect(0.35f, 0.86f, 0.3f, 0.1f), creerFonction(setSceneWrapper, FONCTION_PARAMS(context->sceneController, "menuPrincipal"))), BUTTON_TYPE);
+    ADD_OBJECT_TO_SCENE(scene, NULL, FRAME_DISPLAY_TYPE);
 
     sceneRegisterFunction(scene, BUTTON_TYPE, HANDLE_INPUT, handleInputButtonWrapper, 1, FONCTION_PARAMS(context->input));
     sceneRegisterFunction(scene, VOLUME_TYPE, HANDLE_INPUT, handleInputButtonVolummeWrapper, 1, FONCTION_PARAMS(context->input));
@@ -58,6 +60,9 @@ t_scene* createOptionMenu(t_context* context) {
     sceneRegisterFunction(scene, BUTTON_TYPE, RENDER_UI, renderButtonWrapper, 1, FONCTION_PARAMS(context->renderer));
     sceneRegisterFunction(scene, TEXTE_TYPE, RENDER_UI, renderTextWrapper, 1, FONCTION_PARAMS(context->renderer));
     sceneRegisterFunction(scene, VOLUME_TYPE, RENDER_UI, renderBarreVolummeWrapper, 1, FONCTION_PARAMS(context->renderer));
+
+    sceneRegisterFunction(scene, FRAME_DISPLAY_TYPE, UPDATE, updateFPSDisplayWrapper, -1, FONCTION_PARAMS(context->fpsDisplay, context->frameData, context->renderer));
+    sceneRegisterFunction(scene, FRAME_DISPLAY_TYPE, RENDER_UI, renderFPSDisplayWrapper, -1, FONCTION_PARAMS(context->renderer, context->fpsDisplay));
 
     return scene;
 }
