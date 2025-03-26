169c4352e18aeed5ccc3b0516135f38f3068c8a6
Barth <derouaultbarthelemy@protonmail.com>
2025-03-26 01:47:19 +0100

    j'ai oublié le barrel

--- src/engine/entities/tilesEntity/barrel.h
+++ src/engine/entities/tilesEntity/barrel.h
@@ -1,11 +1,13 @@
 #ifndef BARREL_H
 #define BARREL_H
 
+#include "../../../utils/timer.h"
+#include "../components/health/health.h"
 #include "../tilesEntity.h"
 
 typedef struct {
     t_tileEntity base;
-    int health;
+    t_healthSystem health;
     SDL_bool isExploding;
 } t_barrel;
 
@@ -13,5 +15,6 @@ t_tileEntity* createBarrelEntity(t_tileset* tileset, t_scene* scene);
 void updateBarrel(t_tileEntity* entity, float* deltaTime, t_grid* grid, t_objectManager* entities, t_input* input);
 void renderBarrel(SDL_Renderer* renderer, t_tileEntity* entity, t_camera* camera);
 void takeDamageBarrel(t_tileEntity* entity, float damage);
+void onBarrelDestroy(void* entity);
 
 #endif
