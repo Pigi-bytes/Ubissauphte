7f19aa8fa8c623a3d85e9d89657dc5303f456a94
Baptcarf <baptiste.carfantan72@gmail.com>
2025-04-04 15:41:46 +0200

    ajout d'un fond d'écran

--- src/_scene/mainMenu.c
+++ src/_scene/mainMenu.c
@@ -1,5 +1,24 @@
 #include "mainMenu.h"
 
+void freeTexture(void* elt) {
+    free((SDL_Texture*)elt);
+}
+
+void afficheImage(t_fonctionParam* fp) {
+    t_context* context = GET_PTR(fp, 0, t_context*);
+    SDL_Texture* texture = GET_PTR(fp, 1, SDL_Texture*);
+
+    SDL_RenderClear(context->renderer);
+
+    // Créer un rectangle de destination pour l'image, prenant toute la fenêtre
+    SDL_Rect destRect = {0, 0, window_width, window_height};
+    Uint8 mod = (Uint8)(255 * 0.8);
+    SDL_SetTextureColorMod(texture, mod, mod, mod);
+
+    // Afficher l'image en la redimensionnant pour remplir toute la fenêtre
+    SDL_RenderCopy(context->renderer, texture, NULL, &destRect);
+}
+
 void modifierDifficulty(t_fonctionParam* fonction) {
     t_context* context = GET_PTR(fonction, 0, t_context*);
     t_text** text = GET_PTR(fonction, 1, t_text**);
@@ -43,6 +62,9 @@ t_scene* createMainMenu(t_context* context, t_joueur** player) {
     const uint8_t BUTTON_TYPE = registerType(registre, freeButton, "button");
     const uint8_t TEXTE_TYPE = registerType(registre, freeText, "text");
     const uint8_t FRAME_DISPLAY_TYPE = registerType(registre, freeFPSDisplay, "frameData");
+    const uint8_t FOND_TYPE = registerType(registre, freeTexture, "fond");
+
+    SDL_Texture* texture = loadImage("./assets/imgs/fondEcran.bmp", context->renderer);
 
     char Newtext[50];
     sprintf(Newtext, "difficulté : %s", indiceFromDifficulty(context->difficulty));
@@ -52,9 +74,10 @@ t_scene* createMainMenu(t_context* context, t_joueur** player) {
     addPamaretre(fonctionDiff, FONCTION_PARAMS(context, &(difficultyButton->label)));
     t_scene* scene = createScene(initObjectManager(registre), "menuPrincipal");
 
-    t_text* text = createText(context->renderer, "Lo Poti Donjon", context->font, WHITE);
+    t_text* text = createTextOutline(context->renderer, "Lo Poti Donjon", context->font, BLACK, WHITE, 3);
     text->rect = creerRect((1 - 0.8f) / 2, 0.1f, 0.8f, 0.2f);
 
+    ADD_OBJECT_TO_SCENE(scene, texture, FOND_TYPE);
     ADD_OBJECT_TO_SCENE(scene, text, TEXTE_TYPE);
     ADD_OBJECT_TO_SCENE(scene, createButton(createTextOutline(context->renderer, "Jouer", context->font, BLACK, WHITE, 2), MAGENTA, BLUE, creerRect(0.20f, 0.35f, 0.3f, 0.1f), creerFonction(CreateNiveauWrapper, FONCTION_PARAMS(context, &context->nbLevel, player))), BUTTON_TYPE);
     ADD_OBJECT_TO_SCENE(scene, difficultyButton, BUTTON_TYPE);
@@ -62,6 +85,7 @@ t_scene* createMainMenu(t_context* context, t_joueur** player) {
     ADD_OBJECT_TO_SCENE(scene, createButton(createTextOutline(context->renderer, "Quitter", context->font, BLACK, WHITE, 2), MAGENTA, BLUE, creerRect(0.20f, 0.65f, 0.3f, 0.1f), creerFonction(bouttonClickQuit, FONCTION_PARAMS(context->input))), BUTTON_TYPE);
     ADD_OBJECT_TO_SCENE(scene, NULL, FRAME_DISPLAY_TYPE);
 
+    sceneRegisterFunction(scene, FOND_TYPE, RENDER_UI, afficheImage, 1, FONCTION_PARAMS(context));
     sceneRegisterFunction(scene, BUTTON_TYPE, HANDLE_INPUT, handleInputButtonWrapper, 1, FONCTION_PARAMS(context));
     sceneRegisterFunction(scene, FRAME_DISPLAY_TYPE, UPDATE, updateFPSDisplayWrapper, -1, FONCTION_PARAMS(context->fpsDisplay, context->frameData, context->renderer));
 
